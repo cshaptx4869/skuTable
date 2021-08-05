f819ad3384e05d33d322ca4351cea44f01c8c781
cshaptx4869 <994774638@qq.com>
2021-08-05 09:57:47 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,164 @@
+# SkuTable 组件
+
+## 简介
+
+基于 Layui 的 SkuTable 组件。根据配置动态生成 sku 表。
+
+
+
+## 效果
+
+![Snipaste_2021-08-04_17-38-53.png](https://i.loli.net/2021/08/04/jkaxlJYsnywNUMZ.png)
+
+
+
+## 示例
+
+```html
+<!doctype html>
+<html lang="zh">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>动态SKU表</title>
+    <link rel="stylesheet" type="text/css" href="https://www.layuicdn.com/layui/css/layui.css"/>
+</head>
+<body>
+
+<div class="layui-container">
+    <form action="" class="layui-form">
+        <!-- sku参数表 -->
+        <div class="layui-form-item">
+            <label class="layui-form-label">规格：</label>
+            <div class="layui-input-block">
+                <div class="fairy-spec-table"></div>
+            </div>
+        </div>
+
+        <!-- 动态sku表 -->
+        <div class="layui-form-item">
+            <label class="layui-form-label">SKU表：</label>
+            <div class="layui-input-block">
+                <div class="fairy-sku-table"></div>
+            </div>
+        </div>
+
+        <div class="layui-form-item">
+            <div class="layui-input-block">
+                <button class="layui-btn" lay-submit lay-filter="submit">立即提交</button>
+                <button type="reset" class="layui-btn layui-btn-primary">重置</button>
+            </div>
+        </div>
+    </form>
+</div>
+
+<script src="https://www.layuicdn.com/layui/layui.js"></script>
+<script src="./skuTable.js"></script>
+<script>
+    layui.config({
+        base: './'
+    }).use(['form', 'skuTable'], function () {
+        var form = layui.form, skuTable = layui.skuTable;
+
+        var obj = skuTable.render({
+            //上传占位图
+            skuIcon: 'https://shop.yyhjx.net/backend/assets/abec325a/img/sku-add.png',
+            //上传地址
+            uploadUrl: '',
+            //sku表格配置
+            skuTableConfig: {
+                thead: [
+                    {title: '图片', icon: ''},
+                    {title: '销售价(元)', icon: 'layui-icon-cols'},
+                    {title: '市场价(元)', icon: 'layui-icon-cols'},
+                    {title: '成本价(元)', icon: 'layui-icon-cols'},
+                    {title: '库存', icon: 'layui-icon-cols'},
+                    {title: '状态', icon: ''},
+                ],
+                tbody: [
+                    {type: 'image', field: 'picture', value: '', verify: '', reqtext: ''},
+                    {type: 'input', field: 'price', value: '0', verify: 'required|number', reqtext: '销售价不能为空'},
+                    {type: 'input', field: 'market_price', value: '0', verify: 'required|number', reqtext: '市场价不能为空'},
+                    {type: 'input', field: 'cost_price', value: '0', verify: 'required|number', reqtext: '成本价不能为空'},
+                    {type: 'input', field: 'stock', value: '0', verify: 'required|number', reqtext: '库存不能为空'},
+                    {type: 'select', field: 'status', option: [{key: '启用', value: '1'}, {key: '禁用', value: '0'}], verify: 'required', reqtext: '状态不能为空'},
+                ]
+            },
+            //规格数据
+            specData: [
+                {
+                    id: "1",
+                    title: "颜色",
+                    child: [
+                        {id: "1", title: "红", checked: true},
+                        {id: "2", title: "黄", checked: false},
+                        {id: "3", title: "蓝", checked: false}
+                    ]
+                }, {
+                    id: "2",
+                    title: "尺码",
+                    child: [
+                        {id: "4", title: "S", checked: true},
+                        {id: "5", title: "M", checked: true},
+                        {id: "6", title: "L", checked: false},
+                        {id: "7", title: "XL", checked: false}
+                    ]
+                }, {
+                    id: "3",
+                    title: "款式",
+                    child: [
+                        {id: "8", title: "男款", checked: true},
+                        {id: "9", title: "女款", checked: true}
+                    ]
+                }
+            ],
+            //sku数据
+            //新增的时候为空对象
+            //编辑的时候可以从后台接收，会自动填充sku表，可以去掉注释看效果
+            // skuData: {
+            //     "skus[1-4-8][picture]": "https://cdn.layui.com/upload/2019_5/168_1559291577683_9348.png",
+            //     "skus[1-4-8][price]": "100",
+            //     "skus[1-4-8][market_price]": "200",
+            //     "skus[1-4-8][cost_price]": "50",
+            //     "skus[1-4-8][stock]": "18",
+            //     "skus[1-4-8][status]": "0",
+            //     "skus[1-4-9][picture]": "",
+            //     "skus[1-4-9][price]": "0",
+            //     "skus[1-4-9][market_price]": "0",
+            //     "skus[1-4-9][cost_price]": "0",
+            //     "skus[1-4-9][stock]": "0",
+            //     "skus[1-4-9][status]": "1",
+            //     "skus[1-5-8][picture]": "",
+            //     "skus[1-5-8][price]": "0",
+            //     "skus[1-5-8][market_price]": "0",
+            //     "skus[1-5-8][cost_price]": "0",
+            //     "skus[1-5-8][stock]": "0",
+            //     "skus[1-5-8][status]": "1",
+            //     "skus[1-5-9][picture]": "",
+            //     "skus[1-5-9][price]": "0",
+            //     "skus[1-5-9][market_price]": "0",
+            //     "skus[1-5-9][cost_price]": "0",
+            //     "skus[1-5-9][stock]": "0",
+            //     "skus[1-5-9][status]": "1"
+            // }
+        });
+
+        form.on('submit(submit)', function (data) {
+            //获取规格数据
+            console.log(obj.getSpecData());
+            //获取表单数据
+            console.log(data.field);
+
+            var state = Object.keys(data.field).some(function (item, index, array) {
+                return item.startsWith('skus');
+            });
+            state ? layer.alert(JSON.stringify(data.field), {title: '提交的数据'}) : layer.msg('sku表数据不能为空', {icon: 5, anim: 6});
+            return false;
+        });
+    });
+</script>
+</body>
+</html>
+```
+
---
+++ src/skuTable.js
@@ -0,0 +1,237 @@
+/*
+ * Name: skuTable
+ * Author: cshaptx4869
+ * Project: https://github.com/cshaptx4869/skuTable
+ */
+layui.define(['jquery', 'form', 'upload', 'layer'], function (exports) {
+    "use strict";
+    var $ = layui.jquery,
+        form = layui.form,
+        upload = layui.upload,
+        layer = layui.layer,
+        MOD_NAME = 'skuTable';
+
+    class SkuTable {
+        options = {
+            skuIcon: 'https://shop.yyhjx.net/backend/assets/abec325a/img/sku-add.png',
+            uploadUrl: '',
+            specData: [],
+            skuData: {},
+            skuTableConfig: {
+                thead: [
+                    {title: '图片', icon: ''},
+                    {title: '销售价(元)', icon: 'layui-icon-cols'},
+                    {title: '市场价(元)', icon: 'layui-icon-cols'},
+                    {title: '成本价(元)', icon: 'layui-icon-cols'},
+                    {title: '库存', icon: 'layui-icon-cols'},
+                    {title: '状态', icon: ''},
+                ],
+                tbody: [
+                    {type: 'image', field: 'picture', value: '', verify: '', reqtext: ''},
+                    {type: 'input', field: 'price', value: '0', verify: 'required|number', reqtext: '销售价不能为空'},
+                    {type: 'input', field: 'market_price', value: '0', verify: 'required|number', reqtext: '市场价不能为空'},
+                    {type: 'input', field: 'cost_price', value: '0', verify: 'required|number', reqtext: '成本价不能为空'},
+                    {type: 'input', field: 'stock', value: '0', verify: 'required|number', reqtext: '库存不能为空'},
+                    {type: 'select', field: 'status', option: [{key: '启用', value: '1'}, {key: '禁用', value: '0'}], verify: '', reqtext: ''},
+                ]
+            }
+        };
+
+        constructor(options) {
+            this.options = $.extend(this.options, options);
+            this.renderSpecTable();
+            this.renderSkuTable();
+            this.listen();
+        }
+
+        listen() {
+            var that = this;
+
+            /**
+             * 监听所选规格值的变化
+             */
+            form.on('checkbox(fairy-spec-filter)', function (data) {
+                var specData = [];
+                $.each($('.fairy-spec-table tbody tr'), function () {
+                    var child = [];
+                    $.each($(this).find('input[type=checkbox]'), function () {
+                        child.push({id: $(this).val(), title: $(this).attr('title'), checked: $(this).is(':checked')});
+                    });
+                    var specItem = {id: $(this).find('td').eq(0).data('id'), title: $(this).find('td').eq(0).text(), child: child};
+                    specData.push(specItem);
+                });
+                that.options.specData = specData;
+                that.renderSkuTable();
+            });
+
+            /**
+             * 监听批量赋值
+             */
+            $(document).on('click', '.fairy-sku-table thead tr th i', function () {
+                var that = this;
+                layer.prompt({title: $(that).parent().text().trim() + '批量赋值'}, function (value, index, elem) {
+                    $.each($('.fairy-sku-table tbody tr'), function () {
+                        $(this).find('td').eq($(that).parent().index()).children('input').val(value);
+                    });
+                    layer.close(index);
+                });
+            });
+        }
+
+        /**
+         * 渲染规格表
+         */
+        renderSpecTable() {
+            var table = '<table class="layui-table fairy-spec-table"><thead><tr><th>规格名</th><th>规格值</th></tr></thead><tbody>';
+
+            $.each(this.options.specData, function (index, item) {
+                table += '<tr>';
+                table += `<td data-id="${item.id}">${item.title}</td>`;
+                table += '<td>';
+                $.each(item.child, function (key, value) {
+                    table += `<input type="checkbox" title="${value.title}" lay-filter="fairy-spec-filter" value="${value.id}" ${value.checked ? 'checked' : ''}>`;
+                });
+                table += '</td>';
+                table += '</tr>';
+            });
+
+            table += '</tbody></table>';
+
+            $('.fairy-spec-table').replaceWith(table);
+
+            form.render();
+        }
+
+        /**
+         * 渲染sku表
+         */
+        renderSkuTable() {
+            var that = this, table = '<table class="layui-table fairy-sku-table">';
+
+            if ($('.fairy-spec-table tbody input[type=checkbox]:checked').length) {
+                var prependThead = [], prependTbody = [];
+                $.each(this.options.specData, function (index, item) {
+                    var isShow = item.child.some(function (value, index, array) {
+                        return value.checked;
+                    });
+                    if (isShow) {
+                        prependThead.push(item.title);
+                        var prependTbodyItem = [];
+                        $.each(item.child, function (key, value) {
+                            if (value.checked) {
+                                prependTbodyItem.push({id: value.id, title: value.title});
+                            }
+                        });
+                        prependTbody.push(prependTbodyItem);
+                    }
+                });
+
+                table += '<colgroup><col width="70">'.repeat(prependThead.length + 1) + '</colgroup>';
+
+                table += '<thead>';
+                if (prependThead.length > 0) {
+                    var theadTr = '<tr>';
+
+                    theadTr += prependThead.map(function (t, i, a) {
+                        return '<th>' + t + '</th>';
+                    }).join('');
+
+                    this.options.skuTableConfig.thead.forEach(function (item) {
+                        theadTr += '<th>' + item.title + (item.icon ? ' <i class="layui-icon ' + item.icon + '" style="cursor: pointer;" title="批量赋值"></i>' : '') + '</th>';
+                    });
+
+                    theadTr += '</tr>';
+
+                    table += theadTr;
+                }
+                table += '</thead>';
+
+                var prependTbodyTrs = [];
+                prependTbody.reduce(function (prev, cur, index, array) {
+                    var tmp = [];
+                    prev.forEach(function (a) {
+                        cur.forEach(function (b) {
+                            tmp.push({id: a.id + '-' + b.id, title: a.title + '-' + b.title});
+                        })
+                    });
+                    return tmp;
+                }).forEach(function (item, index, array) {
+                    var tr = '<tr>';
+                    tr += item.title.split('-').map(function (t, i, a) {
+                        return '<td>' + t + '</td>';
+                    }).join('');
+
+                    that.options.skuTableConfig.tbody.forEach(function (c) {
+                        switch (c.type) {
+                            case "image":
+                                tr += '<td><input type="hidden" name="skus[' + item.id + '][' + c.field + ']" value="' + (that.options.skuData[that.makeSkuName(item.id, c.field)] ? that.options.skuData[that.makeSkuName(item.id, c.field)] : c.value) + '" lay-verify="' + c.verify + '" lay-reqtext="' + c.reqtext + '"><img class="fairy-sku-img" style="cursor: pointer;" src="' + (that.options.skuData[that.makeSkuName(item.id, c.field)] ? that.options.skuData[that.makeSkuName(item.id, c.field)] : that.options.skuIcon) + '" alt="' + c.field + '图片"></td>';
+                                break;
+                            case "select":
+                                tr += '<td><select name="skus[' + item.id + '][' + c.field + ']" lay-verify="' + c.verify + '" lay-reqtext="' + c.reqtext + '">';
+                                c.option.forEach(function (o) {
+                                    tr += '<option value="' + o.value + '" ' + (that.options.skuData[that.makeSkuName(item.id, c.field)] == o.value ? 'selected' : '') + '>' + o.key + '</option>';
+                                });
+                                tr += '</select></td>';
+                                break;
+                            case "input":
+                            default:
+                                tr += '<td><input type="text" name="skus[' + item.id + '][' + c.field + ']" value="' + (that.options.skuData[that.makeSkuName(item.id, c.field)] ? that.options.skuData[that.makeSkuName(item.id, c.field)] : c.value) + '" class="layui-input" lay-verify="' + c.verify + '" lay-reqtext="' + c.reqtext + '"></td>';
+                                break;
+                        }
+                    });
+                    tr += '</tr>';
+
+                    tr && prependTbodyTrs.push(tr);
+                });
+
+                table += '<tbody>';
+                if (prependTbodyTrs.length > 0) {
+                    table += prependTbodyTrs.join('');
+                }
+                table += '</tbody>';
+
+            } else {
+                table += '<thead></thead><tbody></tbody>';
+            }
+
+            table += '</table>';
+
+            $('.fairy-sku-table').replaceWith(table);
+
+            form.render();
+
+            upload.render({
+                elem: '.fairy-sku-img',
+                url: this.options.uploadUrl,
+                exts: 'png|jpg|ico|jpeg|gif',
+                accept: 'images',
+                acceptMime: 'image/*',
+                multiple: false,
+                done: function (res) {
+                    if (res.code === 200) {
+                        var url = res.data.url;
+                        $(this.item).attr('src', url).prev().val(url);
+                        layer.msg(res.msg);
+                    } else {
+                        layer.msg(res.msg);
+                    }
+                    return false;
+                }
+            });
+        }
+
+        makeSkuName(value, field) {
+            return 'skus[' + value + '][' + field + ']';
+        }
+
+        getSpecData() {
+            return this.options.specData;
+        }
+    }
+
+    exports(MOD_NAME, {
+        render: function (options) {
+            return new SkuTable(options);
+        }
+    })
+});
